a4316e4ee2b5f7034ba317a416a234324545904e
RubbaBoy <adamyarris@yahoo.com>
2019-01-19 22:45:05 -0500

    Added various APIs Added an API for positioning elements, with relative options, and also added the ability to change the image on the fly

--- OpenTitlebarButtons/Utils/TitlebarButtonHosterForm.cs
+++ OpenTitlebarButtons/Utils/TitlebarButtonHosterForm.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 using System.Windows.Forms;
 using OpenTitlebarButtons.Enums;
 using OpenTitlebarButtons.Native;
-using Vanara.PInvoke;
+using System.Resources;
 using static Vanara.PInvoke.User32_Gdi;
 
 namespace OpenTitlebarButtons.Utils
@@ -12,6 +12,38 @@ namespace OpenTitlebarButtons.Utils
     public class TitlebarButtonHosterForm : PerPixelAlphaWindow
     {
         private const int WmMouseactivate = 0x0021, MaNoactivate = 0x0003;
+        private int _xOffset;
+        private int _yOffset;
+
+        public int XOffset
+        {
+            set
+            {
+                _xOffset = value;
+                Relocate(ParentWindow);
+            }
+            get => _xOffset;
+        }
+
+        public int YOffset
+        {
+            set
+            {
+                _yOffset = value;
+                Relocate(ParentWindow);
+            }
+            get => _yOffset;
+        }
+
+        private Bitmap _image;
+
+        public Bitmap image
+        {
+            get => _image;
+            set => SetBitmap(_image = value);
+        }
+
+        public event EventHandler<CalculateCoordinateEventArgs> CalculateCoords;
 
         private HandleRef _hwndRef;
 
@@ -48,20 +80,39 @@ namespace OpenTitlebarButtons.Utils
 
         private void Attach(NativeUnmanagedWindow parent)
         {
-            parent.WindowChanged += (s, e) => Relocate(this, parent);
+            parent.WindowChanged += (s, e) => Relocate(parent);
             SetWindowPos(new HandleRef(this, Handle), parent.Handle, 0, 0, 0, 0,
                 SetWindowPosFlags.SWP_NOSIZE | SetWindowPosFlags.SWP_NOMOVE);
             NativeThemeUtils.SetWindowLong(Handle, NativeThemeUtils.GWLParameter.GWL_HWNDPARENT,
                 parent.Handle.ToInt32());
-            Relocate(this, parent);
+            Relocate(parent);
         }
 
-        private void Relocate(TitlebarButtonHosterForm frm, NativeUnmanagedWindow parent)
+        private void Relocate(NativeUnmanagedWindow parent)
         {
             var loc = parent.Location;
 
-            SetWindowPos(_hwndRef, (IntPtr) 0, loc.X, loc.Y, 0, 0,
+            var args = new CalculateCoordinateEventArgs(loc.X + XOffset, loc.Y + YOffset, parent.Bounds);
+
+            EventHandler<CalculateCoordinateEventArgs> handler = CalculateCoords;
+            handler?.Invoke(this, args);
+
+            SetWindowPos(_hwndRef, (IntPtr) 0, args.X, args.Y, 0, 0,
                 SetWindowPosFlags.SWP_NOACTIVATE | SetWindowPosFlags.SWP_NOZORDER | SetWindowPosFlags.SWP_NOSIZE);
+        }
+
+        public class CalculateCoordinateEventArgs : EventArgs
+        {
+            public CalculateCoordinateEventArgs(int x, int y, Rectangle bounds)
+            {
+                X = x;
+                Y = y;
+                Bounds = bounds;
+            }
+
+            public int X { get; set; }
+            public int Y { get; set; }
+            public Rectangle Bounds { get; }
         }
     }
 }
